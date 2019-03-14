f360b6c538d5836b1f666525bc40e4691fab67a6
Alsharif Abdullah M <ammsh92@gmail.com>
2019-03-14 12:12:12 +0300

    Initial Commit

---
+++ SnapchatClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapchatClone
+//
+//  Created by Alsharif Abdullah M on 3/14/19.
+//  Copyright © 2019 Alsharif Abdullah M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
